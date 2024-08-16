d2e503c3ba5c12ff90b80ef5a77c9db44940024b
Isadora e Joao Marcelo <Isadoralyz3@gmail.com>
2024-08-16 10:10:01 -0300

    Tarefa feita dia 16/08/24 sobre digitar usuarios da pessoa

--- AppAcademia.java
+++ AppAcademia.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Scanner;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -27,47 +28,88 @@ public class AppAcademia{
 
         Aluno Leal = new Aluno ("Leal", "intermediario", "Feminino",  "14/07/2007", 1.62, 52.0);
 
-            System.out.println(Hacker.getnome());
-            System.out.println(Hacker.gettelefone());
-            System.out.println(Hacker.getendereco());
+        System.out.println(Hacker.getnome());
+        System.out.println(Hacker.gettelefone());
+        System.out.println(Hacker.getendereco());
 
-            Hacker.setnome("nome");
-            Hacker.settelefone("telefone");
-            Hacker.setendereco("endereco");
+        Hacker.setnome("nome");
+        Hacker.settelefone("telefone");
+        Hacker.setendereco("endereco");
 
-            Hacker.alunos.add(Lyz);
-            Hacker.alunos.add(Lyzo);
-            Hacker.alunos.add(Analice);
+        Hacker.alunos.add(Lyz);
+        Hacker.alunos.add(Lyzo);
+        Hacker.alunos.add(Analice);
 
-            System.out.println(Hacker.alunos.get(0).getNome());
-            System.out.println(Hacker.alunos.get(1).getNome());
-            System.out.println(Hacker.alunos.get(2).getNome());
+        System.out.println(Hacker.alunos.get(0).getNome());
+        System.out.println(Hacker.alunos.get(1).getNome());
+        System.out.println(Hacker.alunos.get(2).getNome());
 
+        System.out.println("-----------------------------------");
+
+        System.out.println(Torrance.getnome());
+        System.out.println(Torrance.gettelefone());
+        System.out.println(Torrance.getendereco());
+
+        Torrance.setnome("nome");
+        Torrance.settelefone("telefone");
+        Torrance.setendereco("endereco");
+
+        Torrance.alunos.add(Heloisa);
+        Torrance.alunos.add(Bruna);
+        Torrance.alunos.add(Leal);
+
+        System.out.println(Torrance.alunos.get(0).getNome());
+        System.out.println(Torrance.alunos.get(1).getNome());
+        System.out.println(Torrance.alunos.get(2).getNome());
+
+        for (Aluno alunos : Hacker.alunos){
+            System.out.println(alunos.toString());
+        }
+        for (Aluno alunos : Torrance.alunos){
+            System.out.println(alunos.toString());
+        }
+        System.out.println("Hacker");
+        for(int i = 0 ; i< Hacker.alunos.size(); i++){
             System.out.println("-----------------------------------");
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite o nome da Pessoa");
+            Hacker.alunos.get(i).nome = scan.next();
+            System.out.println("Digite o genero da Pessoa");
+            Hacker.alunos.get(i).genero = scan.next();
+            System.out.println("Digite o nascimento da Pessoa");
+            Hacker.alunos.get(i).nascimento = Hacker.alunos.get(i).convertStringToDate(scan.next());
+            System.out.println("Digite a graduação da Pessoa");
+            Hacker.alunos.get(i).graduacao = scan.next();
+            System.out.println("Digite o peso da Pessoa");
+            Hacker.alunos.get(i).peso = scan.nextDouble();
+            System.out.println("Digite a altura da Pessoa");
+            Hacker.alunos.get(i).altura = scan.nextDouble();
+        }
 
-            System.out.println(Torrance.getnome());
-            System.out.println(Torrance.gettelefone());
-            System.out.println(Torrance.getendereco());
+        for (Aluno alunos : Hacker.alunos){
+            System.out.println(alunos.toString());
+        }
 
-            Torrance.setnome("nome");
-            Torrance.settelefone("telefone");
-            Torrance.setendereco("endereco");
+        System.out.println("Torrance");
+        for(int i = 0 ; i< Hacker.alunos.size(); i++){
+            System.out.println("-----------------------------------");
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Digite o nome da Pessoa");
+            Torrance.alunos.get(i).nome = scan.next();
+            System.out.println("Digite o genero da Pessoa");
+            Torrance.alunos.get(i).genero = scan.next();
+            System.out.println("Digite o nascimento da Pessoa");
+            Torrance.alunos.get(i).nascimento = Torrance.alunos.get(i).convertStringToDate(scan.next());
+            System.out.println("Digite a graduação da Pessoa");
+            Torrance.alunos.get(i).graduacao = scan.next();
+            System.out.println("Digite o peso da Pessoa");
+            Torrance.alunos.get(i).peso = scan.nextDouble();
+            System.out.println("Digite a altura da Pessoa");
+            Torrance.alunos.get(i).altura = scan.nextDouble();
+        }
 
-            Torrance.alunos.add(Heloisa);
-            Torrance.alunos.add(Bruna);
-            Torrance.alunos.add(Leal);
-
-            System.out.println(Torrance.alunos.get(0).getNome());
-            System.out.println(Torrance.alunos.get(1).getNome());
-            System.out.println(Torrance.alunos.get(2).getNome());
-
-            for (Aluno alunos : Hacker.alunos){
-                System.out.println(alunos.toString());
-            }
-            for (Aluno alunos : Torrance.alunos){
-                System.out.println(alunos.toString());
-            }
-
-
-     }
+        for (Aluno alunos : Torrance.alunos){
+            System.out.println(alunos.toString());
+        }
+    }
 }
